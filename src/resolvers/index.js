cd567a5e6bf203d5fb169de930831a86fb3a7489
Chi Hao Poon <karvex@hotmail.com>
2020-05-06 22:31:13 +0200

    remove some code

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,27 +1,7 @@
-const advertisers = require('./advertisers')
-const clusters = require('./clusters')
-const dbExample = require('./db-example')
-const heatmaps = require('./heatmaps')
-const jsonExample = require('./json-example')
-const listings = require('./listings')
-const listingsOverTime = require('./listings-over-time')
-const locations = require('./locations')
-const managers = require('./managers')
-const properties = require('./properties')
-const search = require('./search')
+const advertisers = require("./advertisers");
 
 module.exports.resolvers = {
   Query: {
     ...advertisers,
-    ...clusters,
-    ...dbExample,
-    ...heatmaps,
-    ...jsonExample,
-    ...listings,
-    ...listingsOverTime,
-    ...locations,
-    ...managers,
-    ...properties,
-    ...search,
   },
-}
+};
